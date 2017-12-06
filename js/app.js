a6c9c288ca9415db296749b62f93a072c2f32e6d
carolina-silvag <carolinasguzman@gmail.com>
2017-12-06 08:52:29 -0300

    version con bootstrap

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 // La funcionalidad de tu proyecto
+var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
